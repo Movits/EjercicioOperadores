c0769c7fd7fc345a84bedc32c0774a56ac54910e
Roberto Alagemovits <robertomovits@gmail.com>
2023-06-23 09:41:05 -0300

    Ej1 actualizado

--- Ej1.js
+++ Ej1.js
@@ -6,15 +6,15 @@ let HechizosMin = HechizosTotal * 80 / 100;
 console.log("Los hechizos minimos requeridos para aprobar son: " + HechizosMin);
 
 if (HechizosJerry >= HechizosMin) {
-    console.log ("Jerry puede aprobar")
+    console.log ("Jerry puede aprobar");
 }
 else {
-    console.log("Jerry no puede aprobar")
+    console.log("Jerry no puede aprobar");
 }
 
 if (HechizosTom >= HechizosMin) {
-    console.log ("Tom puede aprobar")
+    console.log ("Tom puede aprobar");
 }
 else {
-    console.log("Tom no puede aprobar")
+    console.log("Tom no puede aprobar");
 }
